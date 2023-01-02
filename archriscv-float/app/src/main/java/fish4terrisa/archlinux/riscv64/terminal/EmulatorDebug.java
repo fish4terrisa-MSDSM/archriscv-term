b05fa55687ec328f2ed7ba1fe325742ff1e43388
fish4terrisa-MSDSM <63049888+fish4terrisa-MSDSM@users.noreply.github.com>
2023-01-02 13:55:35 +0800

    Delete archriscv-float directory

--- archriscv-float/app/src/main/java/fish4terrisa/archlinux/riscv64/terminal/EmulatorDebug.java
+++
@@ -1,8 +0,0 @@
-package fish4terrisa.archlinux.riscv64.terminal;
-
-import android.util.Log;
-
-public final class EmulatorDebug {
-    /** The tag to use with {@link Log}. */
-    public static final String LOG_TAG = ":AlpineTerm";
-}
